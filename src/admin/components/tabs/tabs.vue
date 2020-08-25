ffc2fec1b02073362abb14ecc1d7eb7d10d9ea3b
Mikhail Park <packmisha@mail.ru>
2020-08-25 22:10:41 +0300

    router done

--- src/admin/components/tabs/tabs.vue
+++ src/admin/components/tabs/tabs.vue
@@ -3,30 +3,23 @@
         .container
             ul.tabs
                     li(
-                        v-for="link in links" 
-                        :key="link.id"
-                        :class="['item', {active: link.active}]"
+                        :class="['item']"
                     ).tab
-                        a(
-                            :href="`/${link.alias}`" 
-                            class="tab-link"
-                            @click="link.active = false"
-                        ) {{link.title}}
+                       router-link(to="/").tab-link Обо мне
+                    li(
+                        :class="['item']"
+                    ).tab
+                       router-link(to="/works").tab-link Работы
+                    li(
+                        :class="['item']"
+                    ).tab
+                       router-link(to="/reviews").tab-link Отзывы
 </template>
 
 <script>
-const links = [
-    {id: 0, title: "Обо мне", alias: "admin#/", active: false},
-    {id: 1, title: "Работы", alias: "admin#/works", active: false},
-    {id: 2, title: "Отзывы", alias: "admin#/reviews", active: false}
-]
 
 export default {
-    data() {
-        return {
-            links
-        }
-    }
+   
 }
 </script>
 
